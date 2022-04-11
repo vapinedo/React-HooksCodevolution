0aeb81925463ffbdf74351e11595e4e097370f10
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-11 16:16:01 -0500

    [ADD] conditional effect into Class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import EffectWithClassComponent from './components/02-useEffect/EffectWithClassComponent';
 import UseEffect from './components/02-useEffect/UseEffect';
 
 function App() {
   return (
     <div className="App">
-      <UseEffect />
+      {/* <UseEffect /> */}
+      <EffectWithClassComponent />
     </div>
   );
 }
--- src/components/02-useEffect/EffectWithClassComponent.js
+++ src/components/02-useEffect/EffectWithClassComponent.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class EffectWithClassComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      name: "",
+    };
+  }
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            count: 0
-        }
-    }
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times`;
+  }
 
-    componentDidMount( ) {
-        document.title = `Clicked ${this.state.count} times`
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+        console.log('Updating document title')
+        document.title = `Clicked ${this.state.count} times`;
     }
+  }
 
-    componentDidUpdate() {
-        document.title = `Clicked ${this.state.count} times`
-    }
-
-    render() {
-        return (
-            <div>
-                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-                    Click {this.state.count} times
-                </button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={(e) => {
+            this.setState({ name: e.target.value });
+          }}
+        />
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          Click {this.state.count} times
+        </button>
+      </div>
+    );
+  }
 }
 
-export default EffectWithClassComponent
+export default EffectWithClassComponent;
