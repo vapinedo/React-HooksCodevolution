319e9c6a15f282b51cab845ba24d3ee23362abec
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-11 09:33:22 -0500

    [ADD]  useState with previous state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import HookCounter2 from './components/HookCounter2';
 // import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
+// import HookCounter from './components/HookCounter';
 
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter /> */}
-      <HookCounter />
+      {/* <HookCounter /> */}
+      <HookCounter2 />
     </div>
   );
 }
