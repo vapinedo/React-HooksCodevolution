cc09f1241cea4a015906c15ab08151f9b60e26e6
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-11 09:55:19 -0500

    [ADD] useState with object

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import HookCounter2 from './components/HookCounter2';
+import HookCounter3 from './components/HookCounter3';
+// import HookCounter2 from './components/HookCounter2';
 // import ClassCounter from './components/ClassCounter';
 // import HookCounter from './components/HookCounter';
 
@@ -8,7 +9,8 @@ function App() {
     <div className="App">
       {/* <ClassCounter /> */}
       {/* <HookCounter /> */}
-      <HookCounter2 />
+      {/* <HookCounter2 /> */}
+      <HookCounter3 />
     </div>
   );
 }
