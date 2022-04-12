2b15fd668d0011c9991a78d1a3febdfc8072b4b3
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-12 15:22:01 -0500

    [ADD]  useEffect with incorrect dependency

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import './App.css';
-// import ClassMouse from './components/02-useEffect/ClassMouse';
-// import HookMouse from './components/02-useEffect/HookMouse';
-// import EffectWithClassComponent from './components/02-useEffect/EffectWithClassComponent';
-// import UseEffect from './components/02-useEffect/UseEffect';
-import MouseContainer from './components/02-useEffect/MouseContainer';
+import IntervalHookCounter from './components/02-useEffect/IntervalHookCounter';
+// import IntervalClassCounter from './components/02-useEffect/IntervalClassCounter';
 
 function App() {
   return (
     <div className="App">
-      {/* <UseEffect /> */}
-      {/* <EffectWithClassComponent /> */}
-      {/* <ClassMouse /> */}
-      {/* <HookMouse /> */}
-      <MouseContainer />
+      {/* <IntervalClassCounter /> */}
+      <IntervalHookCounter />
     </div>
   );
 }
