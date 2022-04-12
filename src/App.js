355918314acca3557733efc5c62d7341af2f8a77
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-12 12:56:53 -0500

    [ADD] Run effects only once

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import EffectWithClassComponent from './components/02-useEffect/EffectWithClassComponent';
-import UseEffect from './components/02-useEffect/UseEffect';
+import ClassMouse from './components/02-useEffect/ClassMouse';
+import HookMouse from './components/02-useEffect/HookMouse';
+// import EffectWithClassComponent from './components/02-useEffect/EffectWithClassComponent';
+// import UseEffect from './components/02-useEffect/UseEffect';
 
 function App() {
   return (
     <div className="App">
-      <UseEffect />
+      {/* <UseEffect /> */}
       {/* <EffectWithClassComponent /> */}
+      {/* <ClassMouse /> */}
+      <HookMouse />
     </div>
   );
 }
