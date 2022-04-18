0dba19d8db01afddabe0acdeedb04c78b6ffea42
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-18 08:40:12 -0500

    [UPDATE] dataFetching files location

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import DataFetching from './components/03-dataFetching/DataFetching';
+import DataFetching from './components/02-useEffect/DataFetching';
 // import IntervalHookCounter from './components/02-useEffect/IntervalHookCounter';
 // import IntervalClassCounter from './components/02-useEffect/IntervalClassCounter';
 
