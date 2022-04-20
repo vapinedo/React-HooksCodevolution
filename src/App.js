31224d690e03d2325add26c71cfe5e2d9617a8c2
Victor Pinedo <victor.piendo@masglobalconsulting.com>
2022-04-20 15:01:58 -0500

    [ADD]  useReducer (simple state & action)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import './App.css';
-import { createContext } from 'react';
-import ComponentC from './components/03-context/ComponentC';
-// import IntervalHookCounter from './components/02-useEffect/IntervalHookCounter';
-// import IntervalClassCounter from './components/02-useEffect/IntervalClassCounter';
-
-export const UserContext = createContext()
-export const ChannelContext = createContext()
+import CounterOne from './components/04-useReducer/CounterOne';
 
 function App() {
   return (
     <div className="App">
-      {/* <IntervalClassCounter /> */}
-      
-      <UserContext.Provider value={'Valp'}>
-        <ChannelContext.Provider value={'Valp CODM'} >
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      <CounterOne />
     </div>
   );
 }
